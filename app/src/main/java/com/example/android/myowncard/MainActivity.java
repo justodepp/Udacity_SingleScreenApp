b6630e20dab4ecc3ea17c00fca95b88b05a16402
Gianni <gianniandreacavalli@gmail.com>
2017-01-20 11:01:19 +0100

    string and style fix

--- app/src/main/java/com/example/android/myowncard/MainActivity.java
+++ app/src/main/java/com/example/android/myowncard/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.android.myowncard;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbar.setTitle("Bugs - Computer Service");
+        collapsingToolbar.setExpandedTitleColor(ResourcesCompat.getColor(getResources(), android.R.color.transparent, null));
+        //ContextCompat.getColor(getContext(), android.R.color.transparent)
     }
 }
