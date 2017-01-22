8493ad5afe9a5af86500e7c746d64fc1079b7035
justodeeper <gianniandreacavalli@gmail.com>
2017-01-22 17:01:39 +0100

    company description fix

--- app/src/main/java/com/example/android/myowncard/MainActivity.java
+++ app/src/main/java/com/example/android/myowncard/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.android.myowncard;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         collapsingToolbar.setTitle("Bugs - Computer Service");
         collapsingToolbar.setExpandedTitleColor(ResourcesCompat.getColor(getResources(), android.R.color.transparent, null)); //ContextCompat.getColor(getContext(), android.R.color.transparent)
+
+        TextView colorTextView = (TextView)findViewById(R.id.header_card_company_description);
+        String companyString = getResources().getString(R.string.company_description);
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            colorTextView.setText(Html.fromHtml(companyString, Html.FROM_HTML_MODE_LEGACY)); // for 24 api and more
+        } else {
+            colorTextView.setText(Html.fromHtml(companyString)); // or for older api
+        }
 
     }
 }
